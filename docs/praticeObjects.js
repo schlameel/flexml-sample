06a40de12745c8bb1c6ab4f5b81d9ab792661801
Schlameel <Schlameel@Schlameel.com>
2023-07-30 15:47:58 -0700

    Junk

--- docs/praticeObjects.js
+++ docs/praticeObjects.js
@@ -58,3 +58,22 @@
     }
   ]
 }
+
+body: {
+  "OriginalTo": "15187216129",
+  "AccountSid": "",
+  "OriginalFrom": "+19495107548",
+  "Direction": "inbound",
+  "SipHeader_Contact": "<sip:+19495107548@10.1.110.75:5060>",
+  "ApiVersion": "2.0",
+  "CallerName": "",
+  "RequestUrl": "https://5e72-68-4-38-130.ngrok-free.app/api/flexml",
+  "SipHeader_Call-ID": "657a01545c76205a1b81d02820efaee2@12.7.192.27",
+  "PartnerSid": "155b6e31-4aa1-4c28-9c0f-f0156acaf7aa",
+  "EndpointSid": "29e5638c-ef0b-4571-a4c2-e801db7bbe1a",
+  "CallSid": "56859b40-2f15-11ee-8cd2-2207d9efa912",
+  "To": "15187216129",
+  "FinishKey": null,
+  "From": "19495107548",
+  "Digits": "9"
+}
