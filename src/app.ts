06c6a0b68e7b1506858799e60ffbd4e37692277f
Schlameel <Schlameel@Schlameel.com>
2023-07-29 21:19:05 -0700

    Update

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 3000);
 app.listen();
 setRoutes(app);
 app.listen(app.get('port'), () =>
-  console.log(`FlexML Interview Project listening on port ${app.get('port')}`)
+  console.log(`FlexML Sample Project listening on port ${app.get('port')}`)
 );
 
 export {app};
