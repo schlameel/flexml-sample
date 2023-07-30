179e3352a276a88205e6beaa18734a3c8e2ea5a5
Schlameel <Schlameel@Schlameel.com>
2023-07-30 15:46:56 -0700

    Update: changed for method renaming

--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,8 @@ function setRoutes(app: Express): void {
   const router: Router = express.Router();
   const flexMLCtrl = new FlexMLCtrl();
 
-  router.route('/flexml').get(flexMLCtrl.post);
-  router.route('/flexml').post(flexMLCtrl.post);
+  router.route('/flexml').get(flexMLCtrl.introduction);
+  router.route('/flexml').post(flexMLCtrl.introduction);
   router.route('/flexml/joke').post(flexMLCtrl.joke);
 
   // Apply the routes to our application with the prefix /api
