376ccb8e221dd955fe1150ced8dce29d266840e6
Schlameel <Schlameel@Schlameel.com>
2023-07-31 20:43:37 -0700

    Updates: - Restructured tag creation to use new constructors - value -> text - fleXmlPause

--- src/controllers/flexml.ts
+++ src/controllers/flexml.ts
@@ -5,33 +5,42 @@ import {ResponseTag} from '../models/responseTag';
 import {SayTag} from '../models/sayTag';
 import GatherTag from '../models/gatherTag';
 import PlayTag from '../models/playTag';
-import {lookupPhoneNumber, phoneNumberToNiceText} from '../core/flexMlCore';
+import {
+  fleXmlPause,
+  lookupPhoneNumber,
+  phoneNumberAsIndividualNumbers,
+} from '../core/flexMlCore';
 
 class FlexMLCtrl {
   introduction = async (req: Request, res: Response) => {
     try {
       const phoneNumber = req.body['From'];
       const name = await lookupPhoneNumber(phoneNumber);
-      const response = new ResponseTag();
-      const gather = new GatherTag();
-      gather.addAttribute(
-        'action',
-        format({
-          protocol: req.get('host')?.includes('ngrok') ? 'https' : 'http',
-          host: req.get('host'),
-          pathname: req.originalUrl + '/joke',
-        })
-      );
-      gather.addAttribute('numDigits', 1);
-      gather.addAttribute('validDigits', '9');
-      const sayFirst = new SayTag();
-      sayFirst.value = `,,,, Hello and thank you for calling,, you are calling from ${phoneNumberToNiceText(
-        phoneNumber
-      )}`;
-      const saySecond = new SayTag();
-      saySecond.value = `Your name is ${name}`;
-      gather.children = [sayFirst, saySecond];
-      response.children = [gather];
+      const response = new ResponseTag({
+        children: [
+          new GatherTag({
+            attributes: {
+              action: format({
+                protocol: req.protocol,
+                host: req.get('host'),
+                pathname: req.originalUrl + '/joke',
+              }),
+              numDigits: 1,
+              validDigits: '9',
+            },
+            children: [
+              new SayTag({
+                text: `Hello and thank you for calling${fleXmlPause} you are calling from ${phoneNumberAsIndividualNumbers(
+                  phoneNumber
+                )}`,
+              }),
+              new SayTag({
+                text: `Your name is ${name}`,
+              }),
+            ],
+          }),
+        ],
+      });
       res.contentType('application/xml').status(200).send(response.toXml());
     } catch (error: unknown) {
       if (typeof error === 'string') {
@@ -58,12 +67,11 @@ class FlexMLCtrl {
       const response = new ResponseTag({
         children: [
           new SayTag({
-            value:
-              'A horse walks into a bar. the bartender says,, why the long face?',
+            text: `A horse walks into a bar. the bartender says${fleXmlPause} why the long face?`,
           }),
           new PlayTag({
-            value: format({
-              protocol: req.get('host')?.includes('ngrok') ? 'https' : 'http',
+            text: format({
+              protocol: req.protocol,
               host: req.get('host'),
               pathname: '/media/rimshot.mp3',
             }),
@@ -95,7 +103,7 @@ class FlexMLCtrl {
     const response = new ResponseTag({
       children: [
         new SayTag({
-          value: `There was an error.  The error message is,,, ${errorMessage}`,
+          text: `There was an error.  The error message is ${fleXmlPause} ${errorMessage}`,
         }),
       ],
     });
